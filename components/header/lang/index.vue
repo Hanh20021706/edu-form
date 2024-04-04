8e35ff41df6f365f6d11be13e5c46cd0b46bc60b
Hanh20021706 <hanhs8155@gmail.com>
2024-04-04 15:33:40 +0700

    feat: refactor code

--- components/header/lang/index.vue
+++ components/header/lang/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    <select
-      class="bg-white text-black cursor-pointer border-none outline outline-0 transition-all focus:border-t-transparent focus:outline-0 disabled:border-0"
-      v-model="selectedLanguage"
-      @change="changeLanguage"
-    >
-      <option value="vi">Việt Nam</option>
-      <option value="en">English</option>
-    </select>
+  <div class="flex items-center gap-1">
+    <div class="" v-if="selectedLanguage == 'vi'">
+      <img src="~images/home/VN.png" alt="" class="w-[20px]" />
+    </div>
+    <div class="" v-else>
+      <img src="~images/home/en.png" alt="" class="w-[20px] h-[12px]" />
+    </div>
+    <div class="">
+      <select
+        class="bg-white text-black cursor-pointer border-none outline outline-0 transition-all focus:border-t-transparent focus:outline-0 disabled:border-0"
+        v-model="selectedLanguage"
+        @change="changeLanguage"
+      >
+        <option value="vi">Việt Nam</option>
+        <option value="en">English</option>
+      </select>
+    </div>
   </div>
 </template>
 
