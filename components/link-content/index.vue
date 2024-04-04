538b08d9e328bf0aea418bc11877529da72f50c2
Hanh20021706 <hanhs8155@gmail.com>
2024-04-04 10:49:18 +0700

    feat: ui social media

---
+++ components/link-content/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container md:mb-[88px] mb-[22px]">
+    <div class="banner-link px-[46px] py-[36px]">
+      <div class="banner z-10">
+        <h2 class="md:text-[44px] text-white text-[22px] font-medium">
+          Bắt Đầu Ngay: Tạo Biểu Mẫu Khảo Sát của Bạn!
+        </h2>
+        <p
+          class="text-white md:text-[18px] text-[14px] font-medium mt-[17px] md:mb-[110px] mb-[22px] tracking-tight"
+        >
+          Hãy bắt đầu ngay hôm nay bằng cách tạo biểu mẫu khảo sát của bạn!
+          Thuận tiện, linh hoạt và hoàn toàn tùy chỉnh, trang web của chúng tôi
+          sẽ giúp bạn nhanh chóng thu thập ý kiến quan trọng từ đối tượng của
+          mình.
+        </p>
+        <nuxt-link to="/" class="button-primary px-[24px] py-[8px]">
+          Bấm để tạo khảo sát
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+.banner-link {
+  background-image: url("~images/bg-banner.png");
+  border-radius: 10px;
+  height: auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
     <Feature />
     <Slider />
     <Tabs />
+    <SocialMedia />
+    <LinkContent />
   </div>
 </template>
 
