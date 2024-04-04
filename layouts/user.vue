30fe0952bbec47d5797a8a29f7199d4b4a4a8ea7
Hanh20021706 <hanhs8155@gmail.com>
2024-04-04 11:58:33 +0700

    feat: config layout

---
+++ layouts/user.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    this is layout sign in
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
