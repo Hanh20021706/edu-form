40e8e4c474b076d3f1c033642cc9e492c6739a68
Hanh20021706 <hanhs8155@gmail.com>
2024-04-04 10:33:41 +0700

    feat: ui tabs section

--- components/slider/index.vue
+++ components/slider/index.vue
@@ -34,20 +34,6 @@
 export default {
   data() {
     return {
-      items: [
-        {
-          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-        },
-        {
-          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-        },
-        {
-          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-        },
-        {
-          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-        },
-      ],
       data: [
         {
           id: "slide1",
--- components/tabs/index.vue
+++ components/tabs/index.vue
@@ -1,116 +1,126 @@
 <template>
-  <div class="container md:mb-section mb-section-mb">
-    <TitleSection> Tại sao lại chọn VNForm </TitleSection>
-    <v-card>
-      <v-toolbar flat color="primary" dark>
-        <v-toolbar-title>User Profile</v-toolbar-title>
-      </v-toolbar>
-      <v-tabs vertical>
-        <v-tab>
-          <v-icon left> mdi-account </v-icon>
-          Option 1
-        </v-tab>
-        <v-tab>
-          <v-icon left> mdi-lock </v-icon>
-          Option 2
-        </v-tab>
-        <v-tab>
-          <v-icon left> mdi-access-point </v-icon>
-          Option 3
-        </v-tab>
+  <div class="relative">
+    <div class="absolute right-0 top-0 w-[150px] h-[150px]">
+      <img src="~images/tab/subtract.png" alt="" />
+    </div>
+    <div class="container md:mb-section mb-section-mb">
+      <TitleSection> Tại sao lại chọn VNForm </TitleSection>
+      <div
+        class="flex flex-col-reverse gap-[22px] md:grid md:grid-cols-12 md:items-center"
+      >
+        <div class="md:col-span-6">
+          <v-tabs v-model="tab">
+            <v-tab
+              v-for="item in items"
+              :key="item.tab"
+              class="!md:max-w-[600px]"
+              :ripple="false"
+            >
+              <div
+                class="flex items-center gap-[18px] px-[12px] py-[8px] md:px-[20px] md:py-[18px]"
+              >
+                <div class="image-item">
+                  <img :src="item.icon" alt="" class="w-[80px] h-[80px]" />
+                </div>
+                <div class="flex flex-col">
+                  <h3
+                    class="text-[16px] font-bold text-black md:text-[20px] normal-case mb-[16px]"
+                  >
+                    {{ item.name }}
+                  </h3>
+                  <p
+                    class="text-[12px] font-light text-gray normal-case md:text-[16px]"
+                  >
+                    {{ item.desc }}
+                  </p>
+                </div>
+              </div>
+            </v-tab>
+          </v-tabs>
+        </div>
 
-        <v-tab-item>
-          <v-card flat>
-            <v-card-text>
-              <p>
-                Sed aliquam ultrices mauris. Donec posuere vulputate arcu. Morbi
-                ac felis. Etiam feugiat lorem non metus. Sed a libero.
-              </p>
-
-              <p>
-                Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel,
-                lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis
-                non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non
-                nisl sit amet velit hendrerit rutrum.
-              </p>
-
-              <p class="mb-0">
-                Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu.
-                Pellentesque libero tortor, tincidunt et, tincidunt eget, semper
-                nec, quam. Phasellus blandit leo ut odio.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-tab-item>
-        <v-tab-item>
-          <v-card flat>
-            <v-card-text>
-              <p>
-                Morbi nec metus. Suspendisse faucibus, nunc et pellentesque
-                egestas, lacus ante convallis tellus, vitae iaculis lacus elit
-                id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum
-                aliquam libero, non adipiscing dolor urna a orci. Curabitur
-                ligula sapien, tincidunt non, euismod vitae, posuere imperdiet,
-                leo. Nunc sed turpis.
-              </p>
-
-              <p>
-                Suspendisse feugiat. Suspendisse faucibus, nunc et pellentesque
-                egestas, lacus ante convallis tellus, vitae iaculis lacus elit
-                id tortor. Proin viverra, ligula sit amet ultrices semper,
-                ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In
-                hac habitasse platea dictumst. Fusce ac felis sit amet ligula
-                pharetra condimentum.
-              </p>
-
-              <p>
-                Sed consequat, leo eget bibendum sodales, augue velit cursus
-                nunc, quis gravida magna mi a libero. Nam commodo suscipit quam.
-                In consectetuer turpis ut velit. Sed cursus turpis vitae tortor.
-                Aliquam eu nunc.
-              </p>
-
-              <p>
-                Etiam ut purus mattis mauris sodales aliquam. Ut varius
-                tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce
-                fermentum odio nec arcu.
-              </p>
-
-              <p class="mb-0">
-                Donec venenatis vulputate lorem. Aenean viverra rhoncus pede. In
-                dui magna, posuere eget, vestibulum et, tempor auctor, justo.
-                Fusce commodo aliquam arcu. Suspendisse enim turpis, dictum sed,
-                iaculis a, condimentum nec, nisi.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-tab-item>
-        <v-tab-item>
-          <v-card flat>
-            <v-card-text>
-              <p>
-                Fusce a quam. Phasellus nec sem in justo pellentesque facilisis.
-                Nam eget dui. Proin viverra, ligula sit amet ultrices semper,
-                ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In
-                dui magna, posuere eget, vestibulum et, tempor auctor, justo.
-              </p>
-
-              <p class="mb-0">
-                Cras sagittis. Phasellus nec sem in justo pellentesque
-                facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut,
-                mi. Donec quam felis, ultricies nec, pellentesque eu, pretium
-                quis, sem. Nam at tortor in tellus interdum sagittis.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-tab-item>
-      </v-tabs>
-    </v-card>
+        <div class="md:col-span-6">
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="item in items" :key="item.tab">
+              <div class="flex justify-center md:items-center">
+                <img
+                  :src="item.image"
+                  alt=""
+                  class="max-w-[300px] md:max-w-[500px]"
+                />
+              </div>
+            </v-tab-item>
+          </v-tabs-items>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tab: null,
+      items: [
+        {
+          name: "Dễ Dàng & nhanh chóng",
+          desc: "Tham gia khảo sát chỉ trong vài phút. Giao diện thân thiện và đơn giản để bạn tập trung vào ý kiến của mình",
+          image: require("@/assets/images/tab/image-1.png"),
+          icon: require("@/assets/images/tab/tab-1.png"),
+        },
+        {
+          name: "An Toàn và Bảo Mật",
+          desc: "Tham gia khảo sát chỉ trong vài phút. Giao diện thân thiện và đơn giản để bạn tập trung vào ý kiến của mình",
+          icon: require("@/assets/images/tab/tab-2.png"),
+          image: require("@/assets/images/tab/image-2.png"),
+        },
+        {
+          name: "Khảo Sát Nổi Bật",
+          desc: "Tham gia khảo sát chỉ trong vài phút. Giao diện thân thiện và đơn giản để bạn tập trung vào ý kiến của mình",
+          image: require("@/assets/images/tab/image-3.png"),
+          icon: require("@/assets/images/tab/tab-3.png"),
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.v-tabs-bar {
+  height: auto !important;
+}
+.v-slide-group__wrapper {
+  display: block !important;
+}
+
+.v-tabs-slider-wrapper {
+  height: 0 !important;
+}
+.v-slide-group__content,
+.v-tabs-bar__content {
+  flex-direction: column;
+  gap: 32px;
+}
+.v-tab {
+  text-align: left;
+  border: 2px solid #e4e7ec;
+  max-width: 600px;
+  border-radius: 12px;
+  letter-spacing: normal !important;
+}
+.v-tab:before {
+  background-color: #ffff !important;
+}
+.v-tab--active {
+  border-color: #faa51a;
+}
+.v-tab--active img {
+  opacity: 100% !important;
+}
+.image-item img {
+  max-width: none;
+  opacity: 50%;
+}
+</style>
